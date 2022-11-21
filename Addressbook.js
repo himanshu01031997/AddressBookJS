d62f422b56c8ba506e4c61abc73e36d37303b0d7
himanshu01031997 <hs010397@gmail.com>
2022-11-21 22:04:04 +0530

    create contact

---
+++ Addressbook.js
@@ -0,0 +1,32 @@
+class Contact
+{
+    firstName;
+    lastname;
+    address;
+    city;
+    state;
+    zip;
+    phoneno;
+    email;
+
+
+    consturctor(firstName,lastName,address,city,state,zip,phoneno,email)
+    {
+        this.firstName=firstName;
+        this.lastname=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneno=phoneno;
+        this.email=email;
+    }
+}
+let conta1= new Contact('himanshu','singh','bsnl','bsp','cg','454554','83483489','hs@gmail.com');
+console.log(conta1);
+
+
+
+
+
+    
